677b51ea99bf1b52cad110b482e63d543ca6adbb
Bryce <newtonbw@lopers.unk.edu>
2015-10-08 18:48:41 -0500

    find path and parse path

--- Project.c
+++ Project.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <sys/unistd.h>
 
 //Bryce Newton
 //OS Project Part 1
-//Help from Jerry, Sam, Mike, Ian, Sean, and Ben.
+//Help from Sam, Mike, Ian, Jerry, Sean, and Ben. and the linux book...kinda
+//Couldn't have made it without my best friend Google. Thanks, babe.
 //Using VIM sucks donkey balls
 
 char *PATH;
@@ -15,9 +17,9 @@ char** getPaths();
 
 int main(int argC, char **argV, char **envP )
 {
-    printf("Hello, World!"); //for test stuffs
+    printf("Hello, World!\n"); //for test stuffs
 
-    //find the path
+    //find PATH
     PATH = pathFinder(envP);
     printf("%s\n", PATH);
 
@@ -26,10 +28,11 @@ int main(int argC, char **argV, char **envP )
     printf("Address %p\n", &list);
     int listCount = 0;
     while(list[listCount] != NULL)
-         {
-              printf("Base[%d] Address %p Pointer Value %p String=%s\n", listCount, &list[listCount], list[listCount], list[listCount]);
-              listCount++;
-         }
+        {
+            printf("Base[%d] Address %p Pointer Value %p String=%s\n", listCount, &list[listCount], list[listCount], list[listCount]);
+            listCount++;
+        }
+    printf("mama, we made it!");
 
 }
 
@@ -39,15 +42,13 @@ char* pathFinder(char **envP)
     int counterFind=0;
     int x;
 
-    while (envP[counterFind] != NULL)
-    {
-        for(x = 0; x < 4; x++)
-        {
+    while (envP[counterFind] != NULL) {
+        for (x = 0; x < 4; x++) {
             findPath[x] = envP[counterFind][x]; //looking through enviornment   
         }
-        if(strcmp(findPath, "PATH") == 0) //if PATH found in enviorment details
+        if (strcmp(findPath, "PATH") == 0) //if PATH found in enviorment details
         {
-            printf("Find Path \n");
+            printf("Path Found!\n");
             break;
         }
         counterFind++;
@@ -57,72 +58,75 @@ char* pathFinder(char **envP)
 }
 
 char** getPaths() {
-    
-     //first part variables 
-     char *path=PATH;
-     char **parsed;
-     int counterPath = 0;
-     path = 5;
-     int length = strlen(path)+1;
-     int x;
-     
-     //second part variables
-     int charCounter = 0;
-     int arrayCounter = 0;
-     int y;
 
-     //third part variables
-     int stringCounter = 0;
-     int arrayCounter2 = 0;
-     int z;
+    //printf("getPaths\n");
 
-     //PART 1
-     for(x = 0; x<length; x++)
-     {
-          if(path[x] == ':' || path[x] == '\0')
-	  {
-	       counterPath++;
-	  }
-     }
-     counterPath++; //don't understand why. but ben told me to
+    //first part variables
+    char *path = PATH;
+    char **parsed;
+    int counterPath = 0;
+    path += 5;
+    int length = strlen(path)+1;
+    int x;
 
-     path = malloc(counterPath * sizeof(char*)); //allocate memory
+    //second part variables
+    int charCounter = 0;
+    int arrayCounter = 0;
+    int y;
 
-     //PART 2 making sure there is enough memory for string
-     for(y = 0; y < length; y++)
-     {
-          if(path[y] ==':' || path[y] == '\0')
-          {
-               charCounter++;
-	       parsed[counterPath] = malloc(charCounter);
-	       charCounter = 0;
-	       arrayCounter++;
-          }
-	  else
-	  {
-	       charCounter++;
-          }
+    //third part variables
+    int stringCounter = 0;
+    int arrayCounter2 = 0;
+    int z;
 
-     }
-     //PART 3 put strings in the array
-     for(z = 0; z < length; z++)
-     {
-          if(path[z] == ':' || path[z] == '\0')
-               {
-                    stringCounter++;
-                    parsed[arrayCounter2][stringCounter] = '\0';
-                    arrayCounter2++;
-                    stringCounter=0;
-               }
-               else
-               {
-                    parsed[arrayCounter2][stringCounter] = path[z];
-                    stringCounter++;
-               }
+    //PART 1
+    for(x = 0; x<length; x++)
+    {
+        //printf("part1 ");
+        if(path[x] == ':' || path[x] == '\0')
+        {
+            counterPath++;
+        }
+    }
+    counterPath++; //don't understand why. but ben told me to
 
-     }
+    parsed = malloc(counterPath * sizeof(char*)); //allocate memory
 
-     parsed[counterPath] = NULL;
-     return parsed;
+    //PART 2 making sure there is enough memory for string
+    for(y = 0; y < length; y++)
+    {
+        if(path[y] ==':' || path[y] == '\0')
+        {
+            charCounter++;
+            parsed[arrayCounter] = malloc(charCounter);
+            charCounter = 0;
+            arrayCounter++;
+        }
+        else
+        {
+            charCounter++;
+        }
+
+    }
+    //PART 3 put strings in the array
+    for(z = 0; z < length; z++)
+    {
+        if(path[z] == ':' || path[z] == '\0')
+        {
+            stringCounter++;
+            parsed[arrayCounter2][stringCounter] = '\0';
+            arrayCounter2++;
+            stringCounter=0;
+        }
+        else
+        {
+            parsed[arrayCounter2][stringCounter] = path[z];
+            stringCounter++;
+        }
+
+    }
+
+    parsed[counterPath] = NULL;
+    return parsed;
     
 }
